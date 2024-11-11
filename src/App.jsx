3854a12085f5a9fa40d0ca8a7cd326239934295a
AbhayThakurr <95166645+AbhayThakurr@users.noreply.github.com>
2024-11-11 19:20:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Status from "./Components/Status/Status.jsx";
+import Priority from "./Components/Priority.jsx";
+import User from "./Components/User.jsx";
+
+const App = () => {
+  const [Grouping, setGrouping] = useState(localStorage.getItem("grouping"));
+  const [Order, setOrder] = useState(localStorage.getItem("order"));
+  const setGroupingValue = (newValue) => {
+    if (
+      newValue === "status" ||
+      newValue === "priority" ||
+      newValue === "user"
+    ) {
+      setGrouping(newValue);
+    } else {
+      console.error("Invalid grouping value provided:", newValue);
+    }
+  };
+
+  const setOrderingValue = (newValue) => {
+    if (newValue === "Priority" || newValue === "Title") {
+      setOrder(newValue);
+    } else {
+      console.error("Invalid ordering value provided:", newValue);
+    }
+  };
+  let content;
+
+  if (Grouping === "status") {
+    content = <Status order={Order} />;
+  } else if (Grouping === "priority") {
+    content = <Priority order={Order} />;
+  } else {
+    content = <User order={Order} />;
+  }
+  return (
+    <div className="fullBody">
+      <Navbar
+        order={Order}
+        grouping={Grouping}
+        setGroupingValue={setGroupingValue}
+        setOrderingValue={setOrderingValue}
+      ></Navbar>
+      {content}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Status/Status.jsx
@@ -0,0 +1,155 @@
+import "./Status.css";
+import React, { useEffect, useState, useCallback } from "react";
+import axios from "axios";
+import CardStatus from "../Card/CardStatus.jsx";
+import { icons } from "../../assets/assets";
+
+const Status = () => {
+  const [tick, setTick] = useState([]);
+  const [inProgressno, setinProgressno] = useState([]);
+  const [doneno, setdoneno] = useState([]);
+  const [cancelled, setcancelled] = useState([]);
+  const [backlog, setbacklog] = useState([]);
+  const [order, setOrder] = useState(localStorage.getItem("order"));
+  const [users, setUsers] = useState([]);
+  const [todonum, setTodonum] = useState([]);
+
+  const hello = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        "https://api.quicksell.co/v1/internal/frontend-assignment"
+      );
+      setTick(response.data.tickets);
+      setUsers(response.data.users);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }, []);
+
+  const count = useCallback(() => {
+    const groupedTickets = tick.reduce(
+      (acc, ticket) => {
+        switch (ticket.status) {
+          case "Todo":
+            acc.todopre.push(ticket);
+            break;
+          case "Done":
+            acc.donepre.push(ticket);
+            break;
+          case "cancelled":
+            acc.cancelledpre.push(ticket);
+            break;
+          case "Backlog":
+            acc.backlogpre.push(ticket);
+            break;
+          case "In progress":
+            acc.inprogresspre.push(ticket);
+            break;
+          default:
+            break;
+        }
+        return acc;
+      },
+      {
+        todopre: [],
+        donepre: [],
+        cancelledpre: [],
+        backlogpre: [],
+        inprogresspre: [],
+      }
+    );
+
+    const { todopre, donepre, cancelledpre, backlogpre, inprogresspre } =
+      groupedTickets;
+
+    const sortedGroups =
+      order === "Title"
+        ? {
+            todopre: [...todopre].sort((a, b) =>
+              a.title.localeCompare(b.title)
+            ),
+            inprogresspre: [...inprogresspre].sort((a, b) =>
+              a.title.localeCompare(b.title)
+            ),
+            backlogpre: [...backlogpre].sort((a, b) =>
+              a.title.localeCompare(b.title)
+            ),
+            donepre: [...donepre].sort((a, b) =>
+              a.title.localeCompare(b.title)
+            ),
+            cancelledpre: [...cancelledpre].sort((a, b) =>
+              a.title.localeCompare(b.title)
+            ),
+          }
+        : {
+            todopre: [...todopre].sort((a, b) => b.priority - a.priority),
+            inprogresspre: [...inprogresspre].sort(
+              (a, b) => b.priority - a.priority
+            ),
+            backlogpre: [...backlogpre].sort((a, b) => b.priority - a.priority),
+            donepre: [...donepre].sort((a, b) => b.priority - a.priority),
+            cancelledpre: [...cancelledpre].sort(
+              (a, b) => b.priority - a.priority
+            ),
+          };
+
+    setTodonum(sortedGroups.todopre);
+    setbacklog(sortedGroups.backlogpre);
+    setcancelled(sortedGroups.cancelledpre);
+    setdoneno(sortedGroups.donepre);
+    setinProgressno(sortedGroups.inprogresspre);
+  }, [tick, order]);
+
+  useEffect(() => {
+    hello();
+  }, [hello]);
+
+  useEffect(() => {
+    count();
+  }, [count]);
+
+  useEffect(() => {
+    const newOrder = localStorage.getItem("order");
+    if (newOrder !== order) {
+      setOrder(newOrder);
+    }
+  }, [order]);
+
+  const renderCards = (tickets, status) => {
+    return tickets.map((ticket) => {
+      const user = users.find((item) => item.id === ticket.userId);
+      const available = user ? user.available : false;
+
+      return (
+        <CardStatus key={ticket.id} ticket={ticket} available={available} />
+      );
+    });
+  };
+
+  const renderBoard = (title, image, tickets, status) => (
+    <div className="Board">
+      <div className="boardHeading">
+        <img src={image} className="headingImg" alt={title} />
+        <p className="cText">{title}</p>
+        <p className="cText">{tickets.length}</p>
+        <div className="boardHeading" id="pluske">
+          <img src={icons.Add} className="headingImg" alt="add more" />
+          <img src={icons.ThreeDotMenu} className="headingImg" alt="add more" />
+        </div>
+      </div>
+      <div className="Cards">{renderCards(tickets, status)}</div>
+    </div>
+  );
+
+  return (
+    <div className="Boards">
+      {renderBoard("Backlog", icons.Backlog, backlog, "Backlog")}
+      {renderBoard("Todo", icons.ToDo, todonum, "Todo")}
+      {renderBoard("InProgress", icons.InProgress, inProgressno, "In progress")}
+      {renderBoard("Done", icons.Done, doneno, "Done")}
+      {renderBoard("Canceled", icons.Canceled, cancelled, "cancelled")}
+    </div>
+  );
+};
+
+export default Status;
